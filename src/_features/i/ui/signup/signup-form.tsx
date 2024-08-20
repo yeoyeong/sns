61b0b749a3fe3092ee5a7318c4d697176f311dbb
yeoyeong <yeoyyeong@gmail.com>
2024-08-21 02:03:12 +0900

    refactoring : 회원가입 단계별 과정 랜더링 방식 수정

--- src/_features/i/ui/signup/signup-form.tsx
+++ src/_features/i/ui/signup/signup-form.tsx
@@ -6,13 +6,26 @@ import SignupAccountInfo from './signup-form.accountInfo';
 import SignupProfileSetup from './signup-form.profileSetup';
 import SignupSuccess from './signup-form.success';
 
+type StepProps = {
+  setStep: React.Dispatch<React.SetStateAction<number>>;
+};
+
 export default function Signupform() {
   const [step, setStep] = useState(0);
+
+  const steps: {
+    [key: number]: (props: StepProps) => JSX.Element;
+  } = {
+    0: SignupAccountInfo,
+    1: SignupProfileSetup,
+    2: SignupSuccess,
+  };
+
+  const StepComponent = steps[step];
+
   return (
     <ImodalLayout>
-      {step === 0 && <SignupAccountInfo setStep={setStep} />}
-      {step === 1 && <SignupProfileSetup setStep={setStep} />}
-      {step === 2 && <SignupSuccess />}
+      <StepComponent setStep={setStep} />
     </ImodalLayout>
   );
 }
